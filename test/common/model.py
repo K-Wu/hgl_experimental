265a82ef436924138aea2458c59e6ec330e6f22e
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-05-03 02:47:43 -0500

    fix HGT_DGL

--- test/common/model.py
+++ test/common/model.py
@@ -2,6 +2,128 @@ import torch
 from torch import nn
 from hgl import mp
 from typing import Union
+import math
+
+
+# adapted from HGTLayerHetero in [[third_party/OthersArtifacts/graphiler/examples/HGT/HGT_DGL.py]]
+class HGTLayerHetero(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+        node_dict,
+        edge_dict,
+        n_heads=1,
+        dropout=0.2,
+        use_norm=False,
+    ):
+        super(HGTLayerHetero, self).__init__()
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.node_dict = node_dict
+        self.edge_dict = edge_dict
+        self.num_types = len(node_dict)
+        self.num_relations = len(edge_dict)
+        self.total_rel = self.num_types * self.num_relations * self.num_types
+        self.n_heads = n_heads
+        self.d_k = out_dim // n_heads
+        self.sqrt_dk = math.sqrt(self.d_k)
+        self.att = None
+
+        self.k_linears = nn.ModuleList()
+        self.q_linears = nn.ModuleList()
+        self.v_linears = nn.ModuleList()
+        self.a_linears = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        self.use_norm = use_norm
+
+        for t in range(self.num_types):
+            self.k_linears.append(nn.Linear(in_dim, out_dim))
+            self.q_linears.append(nn.Linear(in_dim, out_dim))
+            self.v_linears.append(nn.Linear(in_dim, out_dim))
+            self.a_linears.append(nn.Linear(out_dim, out_dim))
+            if use_norm:
+                self.norms.append(nn.LayerNorm(out_dim))
+
+        self.relation_pri = nn.Parameter(torch.ones(self.num_relations, self.n_heads))
+        self.relation_att = nn.Parameter(
+            torch.Tensor(self.num_relations, n_heads, self.d_k, self.d_k)
+        )
+        self.relation_msg = nn.Parameter(
+            torch.Tensor(self.num_relations, n_heads, self.d_k, self.d_k)
+        )
+        self.skip = nn.Parameter(torch.ones(self.num_types))
+        self.drop = nn.Dropout(dropout)
+
+        nn.init.xavier_uniform_(self.relation_att)
+        nn.init.xavier_uniform_(self.relation_msg)
+
+    def forward(self, G: mp.HeteroGraph, h):
+        
+        node_dict, edge_dict = self.node_dict, self.edge_dict
+        for (srctype, etype, dsttype), sub_graph in G:
+            
+
+            k_linear = self.k_linears[node_dict[srctype]]
+            v_linear = self.v_linears[node_dict[srctype]]
+            q_linear = self.q_linears[node_dict[dsttype]]
+            if len(node_dict) == 1:
+                k = k_linear(h).view(-1, self.n_heads, self.d_k)
+                v = v_linear(h).view(-1, self.n_heads, self.d_k)
+                q = q_linear(h).view(-1, self.n_heads, self.d_k)
+            else:
+                k = k_linear(h[srctype]).view(-1, self.n_heads, self.d_k)
+                v = v_linear(h[srctype]).view(-1, self.n_heads, self.d_k)
+                q = q_linear(h[dsttype]).view(-1, self.n_heads, self.d_k)
+
+            e_id = self.edge_dict[(srctype, etype, dsttype)]
+
+            relation_att = self.relation_att[e_id]
+            relation_pri = self.relation_pri[e_id]
+            relation_msg = self.relation_msg[e_id]
+
+            k = torch.einsum("bij,ijk->bik", k, relation_att)
+            v = torch.einsum("bij,ijk->bik", v, relation_msg)
+
+            sub_graph.src_node["k"] = k
+            sub_graph.dst_node["q"] = q
+            sub_graph.src_node["v_%d" % e_id] = v
+
+            sub_graph.apply_edges(mp.Fn.v_dot_u("q", "k", "t"))
+            attn_score = (
+                sub_graph.edata.pop("t").sum(-1) * relation_pri / self.sqrt_dk
+            )
+            attn_score = edge_softmax(sub_graph, attn_score, norm_by="dst")
+
+            sub_graph.edata["t"] = attn_score.unsqueeze(-1)
+
+        G.multi_update_all(
+            {
+                etype: (fn.u_mul_e("v_%d" % e_id, "t", "m"), fn.sum("m", "t"))
+                for etype, e_id in edge_dict.items()
+            },
+            cross_reducer="mean",
+        )
+
+        new_h = {}
+        for ntype in G.ntypes:
+            """
+            Step 3: Target-specific Aggregation
+            x = norm( W[node_type] * gelu( Agg(x) ) + x )
+            """
+            n_id = node_dict[ntype]
+            alpha = torch.sigmoid(self.skip[n_id])
+            if "t" not in G.nodes[ntype].data:
+                continue
+            t = G.nodes[ntype].data["t"].view(-1, self.out_dim)
+            trans_out = self.drop(self.a_linears[n_id](t))
+            trans_out = trans_out * alpha  # + h[ntype] * (1-alpha) ?
+            if self.use_norm:
+                new_h[ntype] = self.norms[n_id](trans_out)
+            else:
+                new_h[ntype] = trans_out
+        return new_h
 
 
 class GCNLayer(nn.Module):
